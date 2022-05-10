6d9801f7d6f8d834f5e65eaf841a551d7f20840a
lamkimphu258 <lamkimphu258@gmail.com>
2022-05-10 07:18:34 +0700

    Initial commit

---
+++ app/src/main/java/tdtu/android_fundamental/final_project_todo/MainActivity.java
@@ -0,0 +1,14 @@
+package tdtu.android_fundamental.final_project_todo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
